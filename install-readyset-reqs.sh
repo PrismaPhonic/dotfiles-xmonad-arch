d2653aaf9a1054e5a9e40aa13deec7018ab897bb
Peter Farr <Peter@PrismaPhonic.com>
2021-05-03 14:53:24 -1000

    Added known pre-reqs to install script.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- install-readyset-reqs.sh
+++ install-readyset-reqs.sh
@@ -1,2 +1,59 @@
+#!/bin/bash
+
+set -e
+
+echo Welcome to ReadySet! Let\s get you setup!
+read -p 'Clone readyset git repo to: ' gitrepo
+# Replace ~ with $HOME for better compatibility
+gitrepo="${var/#\~/$HOME}"
+
+#####################################
+##    General Work Dependencies    ##
+#####################################
+
+## Start with repo update & pkg upgrade
+sudo pacman -Syu
+
 # Install tailscale for work vpn
 sudo pacman -S --needed --noconfirm tailscale
+
+# enable and start tailscale
+sudo systemctl enable tailscaled.service
+sudo systemctl start tailscaled.service
+
+# Bring tailscale up - note, this will require you clicking a link and
+# authenticating with SSO login
+sudo tailscale up
+
+#####################################
+## ReadySet (Product) Dependencies ##
+#####################################
+
+# Install rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+
+# Install nightly rust
+rustup install nightly
+
+# Install RLS
+rustup component add rls rust-analysis rust-src
+
+# Install all basic deps
+## OpenSSL is already installed by default on Arch systems.
+sudo pacman -S --needed --noconfirm base-devel clang lz4 docker
+
+# Enable and start docker service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+# Add current user to the docker group
+sudo gpasswd -a ${whoami} docker
+
+# Clone readyset repo
+eval cd "${gitrepo}"
+git clone git@github.com:readysettech/readyset.git
+cd readyset
+
+# Build release
+cargo build --release
